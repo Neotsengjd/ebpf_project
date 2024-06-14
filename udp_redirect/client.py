bf781f71a6ef724c3b797e11cc93d16f2e651b44
Brian <brian030128@gmail.com>
2024-06-14 14:18:39 +0800

    complete udp redirect

---
+++ udp_redirect/client.py
@@ -0,0 +1,15 @@
+import socket
+
+HOST = socket.gethostname()
+PORT = 12346
+server_addr = (HOST, PORT)
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+outdata = "hi"
+print('sendto ' + str(server_addr) + ': ' + outdata)
+s.sendto(outdata.encode(), server_addr)
+    
+while True:
+    indata, addr = s.recvfrom(1024)
+    print('recv' + str(indata))
